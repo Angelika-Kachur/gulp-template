455e41d5a509c6c8252bfdc01da6a6f9a623ca31
Alex Kachur <alexander.kachur@gmail.com>
2016-05-26 08:40:22 +0300

    create template

---
+++ gulpfile.js
@@ -0,0 +1,142 @@
+'use strict';
+
+var gulp = require('gulp'),
+    watch = require('gulp-watch'),
+    prefixer = require('gulp-autoprefixer'),
+    uglify = require('gulp-uglify'),
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    rigger = require('gulp-rigger'),
+    cssmin = require('gulp-clean-css'),
+    imagemin = require('gulp-imagemin'),
+    pngquant = require('imagemin-pngquant'),
+    rimraf = require('rimraf'),
+    browserSync = require("browser-sync"),
+    reload = browserSync.reload;
+
+
+var path = {
+    build: { //Куда складывать готовые после сборки файлы
+        html: 'build/',
+        js: 'build/js/',
+        css: 'build/css/',
+        images: 'build/images/',
+        fonts: 'build/fonts/'
+    },
+    src: { //Пути исходников
+        html: 'src/*.html',
+        js: 'src/js/main.js',
+        sass: 'src/sass/main.scss',
+        images: 'src/images/**/*',
+        fonts: 'src/fonts/**/*'
+    },
+    watch: { //За изменением каких файлов будем наблюдать
+        html: 'src/**/*.html',
+        js: 'src/js/**/*.js',
+        sass: 'src/sass/**/*.scss',
+        images: 'src/images/**/*',
+        fonts: 'src/fonts/**/*'
+    },
+    clean: './build'
+};
+
+
+var config = {
+    server: {
+        baseDir: "./build"
+    },
+    tunnel: true,
+    host: 'localhost',
+    port: 9000,
+    logPrefix: "Frontend_Project"
+};
+
+
+gulp.task('html:build', function () {
+    gulp.src(path.src.html) //Выберем файлы по нужному пути
+        .pipe(rigger()) //Прогоним через rigger
+        .pipe(gulp.dest(path.build.html)) //Выплюнем их в папку build
+        .pipe(reload({stream: true})); //И перезагрузим наш сервер для обновлений
+});
+
+
+gulp.task('js:build', function () {
+    gulp.src(path.src.js) //Найдем наш main файл
+        .pipe(rigger()) //Прогоним через rigger
+        .pipe(sourcemaps.init()) //Инициализируем sourcemap
+        .pipe(uglify()) //Сожмем наш js
+        .pipe(sourcemaps.write()) //Пропишем карты
+        .pipe(gulp.dest(path.build.js)) //Выплюнем готовый файл в build
+        .pipe(reload({stream: true})); //И перезагрузим сервер
+});
+
+
+gulp.task('sass:build', function () {
+    gulp.src(path.src.sass) //Выберем наш main.scss
+        .pipe(sourcemaps.init()) //То же самое что и с js
+        .pipe(sass()) //Скомпилируем
+        .pipe(prefixer()) //Добавим вендорные префиксы
+        .pipe(cssmin()) //Сожмем
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(path.build.css)) //И в build
+        .pipe(reload({stream: true}));
+});
+
+
+gulp.task('images:build', function () {
+    gulp.src(path.src.images) //Выберем наши картинки
+        .pipe(imagemin({ //Сожмем их
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()],
+            interlaced: true
+        }))
+        .pipe(gulp.dest(path.build.images)) //И бросим в build
+        .pipe(reload({stream: true}));
+});
+
+
+gulp.task('fonts:build', function() {
+    gulp.src(path.src.fonts)
+        .pipe(gulp.dest(path.build.fonts))
+});
+
+
+gulp.task('build', [
+    'html:build',
+    'js:build',
+    'sass:build',
+    'fonts:build',
+    'images:build'
+]);
+
+
+gulp.task('watch', function(){
+    watch([path.watch.html], function(event, cb) {
+        gulp.start('html:build');
+    });
+    watch([path.watch.sass], function(event, cb) {
+        gulp.start('sass:build');
+    });
+    watch([path.watch.js], function(event, cb) {
+        gulp.start('js:build');
+    });
+    watch([path.watch.images], function(event, cb) {
+        gulp.start('images:build');
+    });
+    watch([path.watch.fonts], function(event, cb) {
+        gulp.start('fonts:build');
+    });
+});
+
+
+gulp.task('webserver', function () {
+    browserSync(config);
+});
+
+
+gulp.task('clean', function (cb) {
+    rimraf('build/*', cb);
+});
+
+gulp.task('default', ['build', 'webserver', 'watch']);
